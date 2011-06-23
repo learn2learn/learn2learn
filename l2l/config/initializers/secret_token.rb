0c431ddd25c1bfa9a19c6f4ca1e76923c0146957
Nirmal Merchant <nirmal_merchant@mckinsey.com>
2011-06-23 11:31:10 -0400

    Correcting the repository root directory

--- l2l/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-L2l::Application.config.secret_token = '2d5959f9a2d13392af263020d804a08cf32a5ccf49117e2c29e3cffbc05b10729053ff473d4f34a2e54648e9483208e349dc7cfba838f78fea98dd9b002068f4'
